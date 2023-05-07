d796c7f9b1a80a0ddbec7739fcadc1ce2fcbc813
dajanaseketa <dajana.seketa@arsfutura.co>
2023-05-07 16:40:17 +0200

    cookbook letter picker

--- src/components/Letter.js
+++
@@ -1,9 +0,0 @@
-const Letter = ({letter, isSelected, onSelect}) => {
-    return (
-        <div className={isSelected ? "letter-selected" : "letter"} onClick={onSelect}>
-            {letter}
-        </div>
-    )
-}
-
-export default Letter;
--- src/pages/Cookbook.js
+++ src/pages/Cookbook.js
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import LetterPicker from "../components/LetterPicker";
-
 const Cookbook = () => {
-    const [selectedLetter, setSelectedLetter] = useState('a');
 
     return (
         <div className='page-container'>
-            <LetterPicker selectedLetter={selectedLetter} setSelectedLetter={setSelectedLetter} />
         </div>
     )
 }
